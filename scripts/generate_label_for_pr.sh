4b8b09c59a5d5706f78d95d775d2b92638c5712a
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-01-21 11:55:38 +0100

    Implement  workflow for adding labels to PRs automatically

---
+++ scripts/generate_label_for_pr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Input string
+input_string="feature/new-feature"
+
+function generate_label_for_pr() {
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <branch_name>" >&2
+    exit 1
+  fi
+  branch_name=$1
+
+  # Check if the string starts with a specific prefix
+  if [[ "$branch_name" == feature/* ]]; then
+    label="Feature"
+  elif [[ "$branch_name" == fix/* ]]; then
+    label="Fix"
+  elif [[ "$branch_name" == chore/* ]]; then
+    label="Chore"
+  elif [[ "$branch_name" == renovate/* ]]; then
+    label="Dependencies"
+  else
+    echo "Branch name is not valid. It should start with feature/, fix/, chore/ or renovate/" >&2
+    exit 1
+  fi
+
+  echo "$label"
+}
+
+generate_label_for_pr $1
