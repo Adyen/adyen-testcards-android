d8ccff14c17cdd7afb77d4bad0c11dc32ee86293
Oscar Spruit <oscar.spruit@adyen.com>
2026-08-03 10:15:55 +0200

    Shrink resources for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
         }
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             isDebuggable = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 
